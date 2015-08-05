78f69eb9e9c58a3777912ae13e33eedd4c7a46e1
liccwarmmen <lcc@lccdeMac.local>
2015-08-04 18:37:51 -0700

    Initial Commit

---
+++ C-Pointer/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  C-Pointer
+//
+//  Created by lcc on 15-8-4.
+//  Copyright (c) 2015年 lcc. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
